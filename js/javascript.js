ce6593aafb5debcef2247f31be9eaa2747d85943
amarton <amarton@umd.edu>
2021-02-23 13:21:27 -0500

    social and merrics

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,7 @@
     $(".icon-twitter").on("click", function(){
 
-      var tweet = "xxx"; //Tweet text
-      var url = "xxx"; //Interactive URL
+      var tweet = "Pushed too far: Overexertion has claimed lives of 22 Division I football players since 2000"; //Tweet text
+      var url = "https://cnsmaryland.org/interactives/spring-2021/pushed-too-far/"; //Interactive URL
 
       var twitter_url = "https://twitter.com/intent/tweet?text="+tweet+"&url="+url+"&tw_p=tweetbutton";
       window.open(twitter_url, 'mywin','left=200,top=200,width=500,height=300,toolbar=1,resizable=0'); return false;
@@ -11,12 +11,13 @@
 
     $(".icon-facebook").on("click", function(){
 
-      var picture = "xxx"; //Picture URL
-      var title = "xxx"; //Post title
-      var description = "xxx"; //Post description
-      var url = "http://www.umd.edu"; //Interactive URL
+      var picture = "https://cnsmaryland.org/interactives/spring-2021/pushed-too-far/images/social.png"; //Picture URL
+      var title = "Pushed too far"; //Post title
+      var description = "Overexertion has claimed lives of 22 Division I football players since 2000"; //Post description
+      var url = "https://cnsmaryland.org/interactives/spring-2021/pushed-too-far/"; //Interactive URL
 
-        var facebook_url = "https://www.facebook.com/dialog/feed?display=popup&link="+url+"&picture="+picture+"&name="+title+"&description="+description+"&redirect_uri=http://www.facebook.com";
-      window.open(facebook_url, 'mywin','left=200,top=200,width=500,height=300,toolbar=1,resizable=0'); return false;
+      var facebook_url = "https://www.facebook.com/dialog/feed?display=popup&app_id=310302989040998&link="+url+"&picture="+picture+"&name="+title+"&description="+description+"&redirect_uri=http://www.facebook.com";
+    window.open(facebook_url, 'mywin','left=200,top=200,width=500,height=300,toolbar=1,resizable=0'); return false;
+
 
     });
